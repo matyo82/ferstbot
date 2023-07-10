e8aca4eb5726929fd6a4069379372157b5368d77
matyo82 <matingarshasbii@gmail.com>
2023-07-10 11:51:40 +0330

    my ferst bot ...

---
+++ main.py
@@ -0,0 +1,47 @@
+from pyrogram import Client, filters
+import setting
+bot = Client(setting.name,
+        api_id=setting.api_id,
+        api_hash= setting.api_hash,
+        bot_token = setting.bot_token
+)
+
+@bot.on_message(filters.command('start')& filters.private)
+def command1(bot, message):
+    bot.send_message(message.chat.id, "hi this is ferst command!")
+
+
+@bot.on_message(filters.command('help'))
+def command1(bot, message):
+    message.reply_text("سلام لاشی، فعلا باهام هیچ گوهی نمیتونید بخورید :))")
+
+@bot.on_message(filters.text & filters.private)
+def echo(Client, message):
+    message.reply_text("فعلا کاری بلد نیستم بکنم، منم بازی فقط :))")
+
+
+# welcombot
+GROUP = 'gang_so_gang'
+WELCOME_MESSAGE = 'خوش اومدی دیوث، چرا اومدی اینجا حالا؟'
+
+@bot.on_message(filters.chat(GROUP) & filters.new_chat_members)
+def welcome(client, message):
+    message.reply_text(WELCOME_MESSAGE)
+
+
+#send photo
+@bot.on_message(filters.command('photo'))
+def photo(bot, message):
+    bot.send_photo(message.chat.id, "https://digimovie.red/wp-content/uploads/2022/11/o8J1HvqO6QF1hMTY0eQq7itnUn4.jpg")
+
+
+@bot.on_message(filters.audio & filters.private)
+def audio(bot, message):
+    message.reply(f'the id audio: {message.audio.file_id}')
+
+@bot.on_message(filters.command('audio'))
+def send_audio(bot, message):
+    bot.send_audio(message.chat.id, "CQACAgIAAxkBAAMoZKu5xESW447Z_J80ao9gBDFwRaIAAuklAAL27ahI2MnZXFsem9IeBA")
+
+print('I AM ALIVE')
+bot.run()
